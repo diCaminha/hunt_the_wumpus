b5c921f4844dc5920faaa50e2f41cd98f7165681
diCaminha <dineresc@gmail.com>
2015-11-14 16:01:38 -0600

    changed the logic of generating the graph randomly

--- main.py
+++ main.py
@@ -7,90 +7,59 @@ counterLinks = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 count = 50
 
 
-'''
-	The logic of finding 3 edges randomically for each room was breaking down in 3 for loops.
-		Where the first for will only let create a edge between the current room (i) and the random room (randomNumber) if 
-	both have no edges yet.
-	In the second for loop is the same logic. Only creates the edge if both candidates rooms have 1 edges. And finally for the last for loop,
-	will create a edge if both room candidates have 2 edges.
-		That is pretty much forcing, in each for loop, all the rooms have the same number of edges. Avoind the problem before. where
-		in some point, we're trying to find a edge for a room and all the others have already 3 edges.
-'''
-
-'''
-	Problems:
-		Running some times, I faced this problem:
-		-> Okay, in the first forloop the code found for all the rooms a other room. then, each room has 1 edge. 
-		-> BUT, in the second forloop, the last two rooms missing a edge(the only two rooms with only 1 edge) were already connected
-			in the first forloop!!! So, they cannot be connected anymore! Then, the INFINITE LOOP HAPPENS AGAIN.
-'''
 
 
-#first loop to fill out the first edge for all rooms
-for i in range(1, 21):
-	
+#creating a random sequency of caves
+#the logic is gets a random number between 1 and 20 and then put in the random_sequency_caves, creating a random sequency
+random_sequency_caves = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+for i in range(1,21):
 	done = False
 	while not done:
-		if counterLinks[i] != 0:
+		random_cave = random.randrange(1,21)
+		if not random_cave in random_sequency_caves:
+			random_sequency_caves[i] = random_cave
 			done = True
-		else:
-			randomNumber = random.randrange(1,21)
-			if counterLinks[randomNumber] == 0 and graph[i][randomNumber] == 0 and randomNumber != i: 
-				graph[i][randomNumber] = 1
-				graph[randomNumber][i] = 1
-				counterLinks[i]+=1
-				counterLinks[randomNumber]+=1
-				done = True
-				print (i,randomNumber);
 
-#second loop to fill out the second edge for all rooms
-for i in range(1, 21):
-	
-	done = False
-	while not done:
-		if counterLinks[i] != 1:
-			done = True
-		else:
-			randomNumber = random.randrange(1,21)
-			if counterLinks[randomNumber] == 1 and graph[i][randomNumber] == 0 and randomNumber != i: 
-				graph[i][randomNumber] = 1
-				graph[randomNumber][i] = 1
-				counterLinks[i]+=1
-				counterLinks[randomNumber]+=1
-				done = True
-				print (i,randomNumber);
 
-#third loop to fill out the third edge for all rooms
-for i in range(1, 21):
-	
-	done = False
-	while not done:
-		if counterLinks[i] != 2:
-			done = True
-		else:
-			randomNumber = random.randrange(1,21)
-			if counterLinks[randomNumber] == 2 and graph[i][randomNumber] == 0 and randomNumber != i: 
-				graph[i][randomNumber] = 1
-				graph[randomNumber][i] = 1
-				counterLinks[i]+=1
-				counterLinks[randomNumber]+=1
-				done = True
-				print (i,randomNumber);
-		
+'''
+	Using the random_sequency_caves I did the connections following this logic:
+		1. Starting from the firts element in random_sequency_caves, I connect it with the following 3 caves in its front.
+			so, suppose the random_sequency_caves is (8,16,18,20,10,3,6,14,9,1,19,7,5,13,17,2,15,11,12,4)
+			then, the 8 will be linked to 16,18 and 20.
+			The connection will only be accepted if the both current cave and the cave that it is being connected don't have yet 3 connection. 
+			Once one of them has 3, any connection will be created. 
+			
+'''
 
 
 for i in range(1,21):
-	for j in range(1,21):
-		print(graph[i][j], end=' ')
-	print()	
-
+	if(counterLinks[random_sequency_caves[i]] < 3):
+		graph[random_sequency_caves[i]][random_sequency_caves[i+1]] = 1
+		graph[random_sequency_caves[i+1]][random_sequency_caves[i]] = 1
+		counterLinks[random_sequency_caves[i]]+=1
+		counterLinks[random_sequency_caves[i+1]]+=1
+	if(counterLinks[random_sequency_caves[i]] < 3):
+		graph[random_sequency_caves[i]][random_sequency_caves[i+2]] = 1
+		graph[random_sequency_caves[i+2]][random_sequency_caves[i]] = 1
+		counterLinks[random_sequency_caves[i]]+=1
+		counterLinks[random_sequency_caves[i+2]]+=1
+	if(counterLinks[random_sequency_caves[i]] < 3):
+		graph[random_sequency_caves[i]][random_sequency_caves[i+3]] = 1
+		graph[random_sequency_caves[i+3]][random_sequency_caves[i]] = 1
+		counterLinks[random_sequency_caves[i]]+=1
+		counterLinks[random_sequency_caves[i+3]]+=1
 
 #testing choosing rooms
-
-curently_room = int(input("choose a room"))
-print("you are in ",curently_room)
-print("you can move to: ")
-for i in range(1,21):
-	if(graph[curently_room][i] == 1):
-		print(i,end=' ')
-print()
+game_over = False
+while not game_over:
+	option = int(input("enter the number of the option you want\n"
+		+"1. choose a room to go\n"
+		+"2. guess where's the wumpus\n"))
+	if option == 1:
+		curently_room = int(input("choose a room: "))
+		print("you are in ",curently_room)
+		print("you can move to: ")
+		for i in range(1,21):
+			if(graph[curently_room][i] == 1):
+				print(i,end=' ')
+		print()
