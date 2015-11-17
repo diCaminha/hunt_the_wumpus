f2d2f94519342d8f8cf544bc35396b81b2f787f6
diCaminha <dineresc@gmail.com>
2015-11-17 17:24:08 -0600

    changed again the logic of generating the graph. Now it's completely working

--- main.py
+++ main.py
@@ -1,64 +1,97 @@
 import random
+
+
 # Creates a list containing 20 lists initialized to 0
 graph = [[0 for x in range(21)] for x in range(21)] 
 
-counterLinks = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-count = 50
-
-
-
-
-#creating a random sequency of caves
-#the logic is gets a random number between 1 and 20 and then put in the random_sequency_caves, creating a random sequency
+all_caves = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 random_sequency_caves = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-for i in range(1,21):
-	done = False
-	while not done:
-		random_cave = random.randrange(1,21)
-		if not random_cave in random_sequency_caves:
-			random_sequency_caves[i] = random_cave
-			done = True
-
-
-'''
-	Using the random_sequency_caves I did the connections following this logic:
-		1. Starting from the firts element in random_sequency_caves, I connect it with the following 3 caves in its front.
-			so, suppose the random_sequency_caves is (8,16,18,20,10,3,6,14,9,1,19,7,5,13,17,2,15,11,12,4)
-			then, the 8 will be linked to 16,18 and 20.
-			The connection will only be accepted if the both current cave and the cave that it is being connected don't have yet 3 connection. 
-			Once one of them has 3, any connection will be created. 
-			
-'''
-
+copy_random_sequency_caves = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 for i in range(1,21):
-	if(counterLinks[random_sequency_caves[i]] < 3):
-		graph[random_sequency_caves[i]][random_sequency_caves[i+1]] = 1
-		graph[random_sequency_caves[i+1]][random_sequency_caves[i]] = 1
-		counterLinks[random_sequency_caves[i]]+=1
-		counterLinks[random_sequency_caves[i+1]]+=1
-	if(counterLinks[random_sequency_caves[i]] < 3):
-		graph[random_sequency_caves[i]][random_sequency_caves[i+2]] = 1
-		graph[random_sequency_caves[i+2]][random_sequency_caves[i]] = 1
-		counterLinks[random_sequency_caves[i]]+=1
-		counterLinks[random_sequency_caves[i+2]]+=1
-	if(counterLinks[random_sequency_caves[i]] < 3):
-		graph[random_sequency_caves[i]][random_sequency_caves[i+3]] = 1
-		graph[random_sequency_caves[i+3]][random_sequency_caves[i]] = 1
-		counterLinks[random_sequency_caves[i]]+=1
-		counterLinks[random_sequency_caves[i+3]]+=1
+	cave = random.choice(all_caves)
+	all_caves.remove(cave)
+	random_sequency_caves[i] = cave
+	copy_random_sequency_caves[i] = cave
 
+#removing the number 0 extra in both tuple
+random_sequency_caves.remove(0)
+copy_random_sequency_caves.remove(0)
 
 #Choosing the bats, pits and wumpus
-options_caves = random_sequency_caves
-bats = random.choice(options_caves)
-options_caves.remove(bats)
-pits = random.choice(options_caves)
-options_caves.remove(pits)
-wumpus = random.choice(options_caves)
+bats = random.choice(random_sequency_caves)
+random_sequency_caves.remove(bats)
+pits = random.choice(random_sequency_caves)
+random_sequency_caves.remove(pits)
+wumpus = random.choice(random_sequency_caves)
 
-#testing choosing rooms
+
+#Those represents the layers of the graph, 
+#which is formed by 5 caves in the first layer, 10 in the second, and finally 5 caves in the third
+layer1 = []
+layer2 = []
+layer3 = []
+
+
+########### CHOOSING RANDOMLY THE CAVES FOR EACH LAYER  #################
+
+##choosing the caves for the first layer
+for i in range(5):
+	cave = random.choice(copy_random_sequency_caves)
+	layer1.append(cave)
+	copy_random_sequency_caves.remove(cave)
+
+##choosing the caves for the second layer
+for i in range(10):
+	cave = random.choice(copy_random_sequency_caves)
+	layer2.append(cave)
+	copy_random_sequency_caves.remove(cave)
+
+##choosing the caves for the third layer
+for i in range(5):
+	cave = random.choice(copy_random_sequency_caves)
+	layer3.append(cave)
+	copy_random_sequency_caves.remove(cave)
+
+
+
+############ FILLING THE GRAPH #########################
+
+graph[layer1[0]][layer1[-1]] = 1
+graph[layer1[-1]][layer1[0]] = 1
+
+for i in range(4):
+	graph[layer1[i]][layer1[i+1]] = 1
+	graph[layer1[i+1]][layer1[i]] = 1
+
+graph[layer2[0]][layer2[-1]] = 1
+graph[layer2[-1]][layer2[1]] = 1
+
+for i in range(9):
+	graph[layer2[i]][layer2[i+1]] = 1
+	graph[layer2[i+1]][layer2[i]] = 1
+	
+graph[layer3[0]][layer3[-1]] = 1
+graph[layer3[-1]][layer3[1]] = 1
+
+for i in range(4):
+	graph[layer3[i]][layer3[i+1]] = 1
+	graph[layer3[i+1]][layer3[i]] = 1
+
+
+#connections between layers 1,2 and 3
+for i in range(5):
+	
+	graph[layer1[i]][layer2[2*i]] = 1
+	graph[layer2[2*i]][layer1[i]] = 1
+	
+	graph[layer2[(i*2)+1]][layer3[i]] = 1
+	graph[layer3[i]][layer2[(i*2)+1]] = 1
+
+########### END FILLING THE GRAPH #########
+
+
 game_over = False
 borders = []
 
@@ -115,6 +148,8 @@ while not game_over:
 				print("I fell a draft!")
 			
 	#########################################################
-			print("you can move to: ")
-			print(borders)
+			
+			print("you can move to:",borders)
+			print()
+			
 
