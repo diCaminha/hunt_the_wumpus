b8df5547e68590db4fcfd10ac2f1dac68ebf8ca5
TheAustinG <austingardner23@gmail.com>
2015-11-18 19:27:05 -0600

    Cookies are now accepting the first choice use enters. Still a long ways to go but it's something

--- cgi-bin/htw-web.py
+++ cgi-bin/htw-web.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
+import http.cookies
+import time
 import cgi
 
-print("Content-Type: text/html;charset=utf-8")
-print()
+form = cgi.FieldStorage()
 
-print(
-"""
-<html>
-	<body>
-		<h2>Python made this</h2>
-	</body>
-</html>
-"""
-)
+option = form.getvalue('firstChoice')
+
+print('Content-Type: text/html')
+print()
+print('<html><body>')
+print('User entered option number ', option, '<br />')
+print('Now storing the option in Cookie named \'FirstChoice\'...<br />')
+
+cookie = http.cookies.SimpleCookie()
+cookie['FirstChoice'] = option
+
+print('Now retrieving the option the user entered by printing out the cookie we just created...<br />')
+print(cookie, '<br />')
+
+print('If both numbers match then we have success!<br /><br />')
+
+print('<a href="../start.html"><h3>Click Here to Start Over</h3></a>')
+print('</body></html>')
