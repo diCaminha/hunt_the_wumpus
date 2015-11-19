4a0f1b14eb492ee59b89d7902dc414345471709b
TheAustinG <austingardner23@gmail.com>
2015-11-18 19:05:43 -0600

    Basic starting html page is done.

---
+++ cgi-bin/htw-web.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import cgi
+
+print("Content-Type: text/html;charset=utf-8")
+print()
+
+print(
+"""
+<html>
+	<body>
+		<h2>Python made this</h2>
+	</body>
+</html>
+"""
+)
